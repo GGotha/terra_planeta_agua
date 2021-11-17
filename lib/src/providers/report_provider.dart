45b579f4bf2187341cc792b64a8496341c29c689
ggotha <snakygamer@gmail.com>
2021-11-17 15:46:10 -0300

    add filter

--- lib/src/providers/report_provider.dart
+++ lib/src/providers/report_provider.dart
@@ -4,12 +4,92 @@ import 'package:terra_planeta_agua/src/models/report_model.dart';
 import 'package:terra_planeta_agua/src/providers/dio.dart';
 
 class ReportProvider {
-  Future<void> getReports(reportStore) async {
+  Future<void> getAllReports(reportStore) async {
     final response = await dio.get(
       '/todos',
     );
 
     final waterPH = new Random();
+
+    reportStore.reports.clear();
+
+    if (response.statusCode == 200) {
+      response.data.forEach(
+        (report) => reportStore.add(
+          new ReportModel(
+            id: report['id'],
+            title: report['title'],
+            completed: report['completed'],
+            waterPH: waterPH.nextDouble(),
+            place: "São Paulo",
+          ),
+        ),
+      );
+    } else {
+      throw Exception('Failed to get reports');
+    }
+  }
+
+  Future<void> getTodayReports(reportStore) async {
+    final response = await dio.get(
+      '/todos?id=1',
+    );
+
+    final waterPH = new Random();
+
+    reportStore.reports.clear();
+
+    if (response.statusCode == 200) {
+      response.data.forEach(
+        (report) => reportStore.add(
+          new ReportModel(
+            id: report['id'],
+            title: report['title'],
+            completed: report['completed'],
+            waterPH: waterPH.nextDouble(),
+            place: "São Paulo",
+          ),
+        ),
+      );
+    } else {
+      throw Exception('Failed to get reports');
+    }
+  }
+
+  Future<void> getTwoWeeksReports(reportStore) async {
+    final response = await dio.get(
+      '/todos?userId=10',
+    );
+
+    final waterPH = new Random();
+
+    reportStore.reports.clear();
+
+    if (response.statusCode == 200) {
+      response.data.forEach(
+        (report) => reportStore.add(
+          new ReportModel(
+            id: report['id'],
+            title: report['title'],
+            completed: report['completed'],
+            waterPH: waterPH.nextDouble(),
+            place: "São Paulo",
+          ),
+        ),
+      );
+    } else {
+      throw Exception('Failed to get reports');
+    }
+  }
+
+  Future<void> getOneMonthReports(reportStore) async {
+    final response = await dio.get(
+      '/todos?completed=false',
+    );
+
+    final waterPH = new Random();
+
+    reportStore.reports.clear();
 
     if (response.statusCode == 200) {
       response.data.forEach(
--- lib/src/ui/screens/report_screen.dart
+++ lib/src/ui/screens/report_screen.dart
@@ -9,58 +9,121 @@ import 'package:terra_planeta_agua/src/ui/widgets/report_list.dart';
 class ReportScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List dropdownListItem = ["Hoje", "14 dias", "1 mês", "Todos"];
+
     final reportStore = ReportStore();
 
     return Scaffold(
       backgroundColor: ONGBlueColor,
       appBar: CustomAppBar(
         actions: [
-          Container(
-            margin: EdgeInsets.only(right: 10),
-            child: IconButton(
-              icon: Icon(
+          DropdownButton(
+            underline: Container(
+              height: 0,
+              color: Colors.transparent,
+            ),
+            style: const TextStyle(
+              color: Colors.black,
+            ),
+            elevation: 0,
+            icon: Container(
+              margin: EdgeInsets.only(right: 10),
+              child: Icon(
                 Icons.filter_list,
                 color: Colors.black,
               ),
-              onPressed: () {},
             ),
+            iconSize: 25,
+            onChanged: (dropdownValue) async {
+              if (dropdownValue == "Hoje") {
+                return await ReportProvider().getTodayReports(reportStore);
+              }
+              if (dropdownValue == "14 dias") {
+                return await ReportProvider().getTwoWeeksReports(reportStore);
+              }
+
+              if (dropdownValue == "1 mês") {
+                return await ReportProvider().getOneMonthReports(reportStore);
+              }
+
+              if (dropdownValue == "Todos") {
+                return await ReportProvider().getAllReports(reportStore);
+              }
+            },
+            items: dropdownListItem.map((item) {
+              return DropdownMenuItem(
+                value: item,
+                child: Text(item),
+              );
+            }).toList(),
           )
+          // Container(
+          //   margin: EdgeInsets.only(right: 10),
+          //   child: IconButton(
+          //     icon: Icon(
+          //       Icons.filter_list,
+          //       color: Colors.black,
+          //     ),
+          //     onPressed: () {},
+          //   ),
+          // )
         ],
       ),
-      body: FutureBuilder(
-        future: ReportProvider().getReports(reportStore),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Observer(
-              builder: (_) => Container(
-                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: ListView.builder(
-                  itemCount: reportStore.reports.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 5,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+            child: Text(
+              'Relatórios Sustentáveis',
+              style: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: ReportProvider().getAllReports(reportStore),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Observer(
+                    builder: (_) => Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      child: ListView.builder(
+                        itemCount: reportStore.reports.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 5,
+                            ),
+                            child: Column(
+                              children: [
+                                ReportList(
+                                  reports: reportStore.reports[index],
+                                ),
+                              ],
+                            ),
+                          );
+                        },
                       ),
-                      child: ReportList(
-                        reports: reportStore.reports[index],
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 50),
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.black),
                       ),
-                    );
-                  },
-                ),
-              ),
-            );
-          } else {
-            return Center(
-              child: Container(
-                margin: EdgeInsets.symmetric(vertical: 50),
-                child: CircularProgressIndicator(
-                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.black),
-                ),
-              ),
-            );
-          }
-        },
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
       // body: Observer(
       //   builder: (_) => Container(
